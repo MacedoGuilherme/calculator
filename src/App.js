1677e3dfbc015863cfae8c34b4d6e7d973f38df5
Guilherme Macedo <macedoguilherme96@gmail.com>
2021-06-10 21:27:24 -0300

    add calculator module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
+import { sum } from "./helpers/calculator";
 
 function App() {
+  const number1 = useRef(null);
+  const number2 = useRef(null);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(number1, number2);
+  };
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={(event) => submitHandler(event)}>
         <label htmlFor="number1">Número 1</label>
         <div>
-          <input type="number" name="number1" id="number1" placeholder="Insira o primeiro número" required />
+          <input
+            type="number"
+            name="number1"
+            id="number1"
+            placeholder="Insira o primeiro número"
+            required
+            ref={number1}
+          />
         </div>
 
         <label htmlFor="number2">Número 2</label>
         <div>
-          <input type="number" name="number2" id="number2" placeholder="Insira o segundo número" required />
+          <input
+            type="number"
+            name="number2"
+            id="number2"
+            placeholder="Insira o segundo número"
+            required
+            ref={number2}
+          />
         </div>
 
         <div>
